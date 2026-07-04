27252642e14f3ad1e79625062caddc23eb87a1f7
Thomas F. K. Jorna <hello@tefkah.com>
2026-07-04 18:24:57 +0200

    fix: never prefetch, only on hover

--- web/src/app/(v3)/v3/_/components/v3-link.tsx
+++ web/src/app/(v3)/v3/_/components/v3-link.tsx
@@ -1,19 +1,30 @@
 "use client"
 
 import Link from "next/link"
+import { useRouter } from "next/navigation"
 
 import { useVersionBasePath } from "@v3/_/components/version-context"
 
 export const V3Link = (props: Parameters<typeof Link>[0]) => {
   const basePath = useVersionBasePath()
+  const router = useRouter()
 
   if (typeof props.href === "string") {
     const prefixedHref = props.href.startsWith("/")
       ? `${basePath}${props.href}`
       : props.href
 
-    return <Link {...props} href={prefixedHref} />
+    return (
+      <Link
+        prefetch={false}
+        onPointerEnter={() => {
+          router.prefetch(prefixedHref)
+        }}
+        {...props}
+        href={prefixedHref}
+      />
+    )
   }
 
-  return <Link {...props} />
+  return <Link prefetch={false} {...props} />
 }
